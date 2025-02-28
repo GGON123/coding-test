2dbd83864399ebdddcab3b1fd1fe29e821d30b54
Kang Myoung Gon <jin04053@naver.com>
2025-03-01 04:53:06 +0900

    readline방식 추가

--- print-string/print-string.js
+++ print-string/print-string.js
@@ -1,7 +1,18 @@
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join(__dirname, "input.txt");
+// const fs = require("fs");
+// const path = require("path");
+// const filePath = path.join(__dirname, "input.txt");
 
-const input = fs.readFileSync(filePath, "utf-8").toString().trim();
+// const input = fs.readFileSync(filePath, "utf-8").toString().trim();
 
-console.log(input);
+// console.log(input);
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on("line", function (line) {
+  console.log(line);
+  rl.close();
+});
