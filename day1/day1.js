88936e95c51598884139f063bf24be8fa5e7fa1e
Kang Myoung Gon <jin04053@naver.com>
2025-03-03 03:24:20 +0900

    receipt calculation(영수증 계산)

--- day1/day1.js
+++ day1/day1.js
@@ -12,22 +12,60 @@
 // }
 // console.log(answer);
 
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// let answer = "";
+// rl.on("line", (line) => {
+//   const input = line.split(" ");
+
+//   if (input.length === 2) {
+//     answer += parseInt(input[0]) + parseInt(input[1]) + "\n";
+//   }
+// }).on("close", () => {
+//   console.log(answer);
+//   process.exit();
+// });
+
+// -----------------------------------------------------------------
+
+// const fs = require("fs");
+// const path = require("path");
+// const filePath = path.join(__dirname, "input.txt");
+// const input = fs.readFileSync(filePath, "utf-8").toString().split("\n");
+
+// const totalCount = parseInt(input[0].trim());
+// const N = parseInt(input[1].trim());
+
+// let result = 0;
+// for (let i = 2; i < N + 2; i++) {
+//   const tmp = input[i].trim().split(" ");
+//   result += parseInt(tmp[0]) * parseInt(tmp[1]);
+// }
+// console.log(result === totalCount ? "Yes" : "No");
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let answer = "";
+const input = [];
 rl.on("line", (line) => {
-  const input = line.split(" ");
-
-  if (input.length === 2) {
-    answer += parseInt(input[0]) + parseInt(input[1]) + "\n";
-  }
+  input.push(line.trim());
 }).on("close", () => {
-  console.log(answer);
+  const totalCount = parseInt(input[0].trim());
+  const N = parseInt(input[1].trim());
+
+  let result = 0;
+  for (let i = 2; i < N + 2; i++) {
+    const tmp = input[i].split(" ");
+    result += parseInt(tmp[0]) * parseInt(tmp[1]);
+  }
+
+  console.log(result === totalCount ? "Yes" : "No");
   process.exit();
 });
-
-// -----------------------------------------------------------------
